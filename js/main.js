1f16df9a2c85638e6e2daed68c25411eccb78f66
ZikDev <victor.bertschy@studentfr.ch>
2023-11-23 10:10:51 +0100

    burger menu terminé

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".nav");
 
+var toggleMenu = function toggleMenu() {
+  menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
+};
+
+burger.addEventListener("click", toggleMenu);
